8589a85c630a2fdd4d77cb6b29cb33b34d4cb9ec
alfred2504 <inforamaiv@gmail.com>
2024-03-13 02:12:14 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SELECT * 
 FROM information_schema.user_privileges 
 WHERE grantee = 'user_0d_1@localhost' OR grantee = 'user_0d_2@localhost';
